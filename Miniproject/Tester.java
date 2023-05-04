21f25731fb222d6b341f37b21a7201b698aea160
67301_utkarsh <utkarshrane456@gmail.com>
2023-05-04 22:12:29 +0530

    update

--- Miniproject/Tester.java
+++ Miniproject/Tester.java
@@ -1,7 +1,12 @@
 package tester;
 
 
-import java.util.ArrayList;
+import static utils.CustomerValidationRule.ValidateAll;
+import static utils.CustomerValidationRule.customerlogin;
+import static utils.Customerutils.customer;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,29 +14,31 @@ import com.app.customer.Customer;
 import com.app.customer.ServicePlan;
 
 import cust_exception.InvalidCustomerException;
-import utils.CustomerValidationRule;
 
 public class Tester {
 
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
 			
-			List<Customer> cust =new ArrayList<>()  ;
+			List<Customer> cust =customer();
 		
 			boolean exit = false;
 			System.out.println("-----Service plans for Customer-------- ");
+			System.out.println();
 			ServicePlan[] sep=ServicePlan.values();
 			for(ServicePlan sp: sep)
 			{
 				System.out.println(sp);
 			}
 			System.out.println("-----------------------------------------");
-			
+			System.out.println();
 			System.out.println("-------Options for Customer--------------");
-			System.out.println("1.sign up\n2.sign in \n3.update pasword \n4.unsubscribe user \n5.Display All customer \n 0.exit");
+			System.out.println("1.sign up\n2.sign in \n3.update pasword \n4.unsubscribe user \n5.Display All customer "
+					+ " \n6.Display All customer in accending \n7.Display All customer sort as per dob n reg amount \n 0.exit");
 
 			
-			while (!exit) {
+			while (!exit)
+			{
 				try {
 					System.out.println("enter choice");
 					switch (sc.nextInt()) {
@@ -39,43 +46,31 @@ public class Tester {
 				
 							System.out.println(
 									"enter  firstname,  lastname,  email,  passwd,  regAmt ,dob (yyyy-MM-dd),plan_name");
-							Customer customer = CustomerValidationRule.ValidateAll(sc.next(), sc.next(), sc.next(),
+							Customer customer = ValidateAll(sc.next(), sc.next(), sc.next(),
 									sc.next(), sc.nextDouble(), sc.next(), sc.next(), cust);
 							cust.add(customer);
 							System.out.println("sucessfully registered!!!!!!!!!!!");
 						
 						break;
 					case 2:
-						System.out.println("enter email and enter password ");
-						Customer cd=new Customer(sc.next(),sc.next());
-						//Customer cgmail=new Customer(sc.next());
+						System.out.println("enter email and password ");
+						customer=customerlogin(sc.next(),sc.next(),cust);
+						System.out.println(customer);
 						
-                        int index= cust.indexOf(cd);
-                        if(index==-1)
-                        {
-                        	throw new InvalidCustomerException("invalid email and password");
-                        }
-                       System.out.println(cust.get(index));
 						break;
 					case 3:
-						System.out.println("enter email and enter password ");
-						 cd=new Customer(sc.next(),sc.next());
-
-					      index=cust.indexOf(cd);
-					      if(index==-1)
-	                        {
-	                        	throw new InvalidCustomerException("invalid email and password");
-	                        }
-					      
-					      System.out.println("enter new password");
-					      cust.get(index).setPasswd(sc.next());
-						System.out.println("new password set sucessful");
+						System.out.println("enter email and password ");
+						customer=customerlogin(sc.next(),sc.next(),cust);
+                        customer.setPasswd(sc.next());
+                        	System.out.println("password set sucessful");
+                        		
+						
 						break;
 					case 4:
-						System.out.println("enter email and enter password ");
-						cd=new Customer(sc.next(),sc.next());
+						System.out.println("enter email  ");
+						Customer cgmail=new Customer(sc.next());
 
-					      index=cust.indexOf(cd);
+						int index=cust.indexOf(cgmail);
 					      if(index==-1)
 	                        {
 	                        	throw new InvalidCustomerException("invalid email and password");
@@ -92,12 +87,36 @@ public class Tester {
 						
 						break;
 					case 6:
+						Collections.sort(cust);
+						break;
+					case 7:
+						Collections.sort(cust, new Comparator<Customer>()
+								{
+
+									@Override
+									public int compare(Customer c1, Customer c2) {
+										int ret=c1.getDob().compareTo(c2.getDob());
+										if(ret==0)
+										return ((Double)c1.getRegAmt()).compareTo(c2.getRegAmt());
+										
+										return ret;
+									}
+							
+								}
+						);
+						System.out.println("sort information by dob ");
+						for(Customer c: cust)
+						System.out.println(c);
+						break;
+					case 0:
 						exit = true;
 						break;
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
+				} 
+				catch (Exception e) {
+				e.printStackTrace();
 					//System.out.println(e.getMessage());
+					//System.out.println(e);
 				}
 			}
 		}
@@ -109,5 +128,5 @@ public class Tester {
 
 //utkarsh rane utk@gmail utk 1000 2000-03-02 silver
 //sham ghire sham@ shamu 5000 2001-01-02 diamond
-//shubham badgujar shu@ subhya 2000 1999-12-25
+//shubham badgujar shu@ subhya 2000 1999-12-25 gold
 
